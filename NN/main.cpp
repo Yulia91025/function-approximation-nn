fe1f5179eea1f118a15924705ebdc63c7d79d33a
Юлия Худеньких <71973418+Yulia91025@users.noreply.github.com>
2021-12-08 19:01:15 +0300

    Added file input

--- NN/main.cpp
+++ NN/main.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
 #include "neural_network.cpp"
+#include <fstream>
+#include <string>
+using namespace std;
 int main()
 {
-	DATA data; // считываем данные 
+	string filename; // имя файла
+	cout << "Enter filename:" << endl;
+	cin >> filename; // считываем имя файла
+	ifstream file(filename);
+	// Если мы не можем открыть файл для чтения его содержимого, то выводим сообщение об ошибке
+	if (!file)
+	{
+		cerr << "This file could not be opened for reading!" << endl;
+		char c;
+		cout << "Please enter any symbol to complete program" << endl;
+		cin >> c; // чтобы программа не закрывалась сразу после вывода ошибки
+		return -1;
+	}
+	DATA data(file); // считываем данные 
 	Neural_network Net(data); // создаём нейросеть 
 	return 0;
 };
